c5b2e65cdeecd1b393b013dc5188f116fffc0385
keerthanacse13@gmail.com <keerthanacse13@gmail.com>
2021-04-22 16:30:06 +0800

    Integration test added

--- src/test/java/com/jooq/jooq/JooqApplicationTests.java
+++ src/test/java/com/jooq/jooq/JooqApplicationTests.java
@@ -1,13 +1,60 @@
 package com.jooq.jooq;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.jooq.jooq.model.Tables;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Record3;
+import org.jooq.Result;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jooq.JooqTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class JooqApplicationTests {
+import java.util.Arrays;
 
-	@Test
-	void contextLoads() {
-	}
+@RunWith(SpringRunner.class)
+@JooqTest
+public class JooqApplicationTests {
 
+    @Autowired
+    private DSLContext dslContext;
+
+    @Before
+    public void insertRecords() {
+
+        dslContext.delete(Tables.CUSTOMER).execute();
+        dslContext.insertInto(Tables.CUSTOMER, Tables.CUSTOMER.NAME, Tables.CUSTOMER.AGE)
+                .values("Ram", 20)
+                .values("raji", 23)
+                .values("seetha", 26).execute();
+    }
+
+    @Test
+    public void totalRecordCountSuccess() throws Exception {
+
+        Result<Record> result = dslContext.select().from(Tables.CUSTOMER).fetch();
+        Assert.assertEquals(3, result.size());
+    }
+
+    @Test
+    public void listOfNamesSuccess() {
+        Result<Record3<Integer, String, Integer>> customerList = dslContext.select(Tables.CUSTOMER.ID, Tables.CUSTOMER.NAME, Tables.CUSTOMER.AGE).from(Tables.CUSTOMER).fetch();
+        Assert.assertEquals(Arrays.asList("Ram", "raji", "seetha"), customerList.getValues(1));
+    }
+
+    @Test
+    public void incorrectAgeEntryFailure() {
+        Result<Record3<Integer, String, Integer>> customerList = dslContext.select(Tables.CUSTOMER.ID, Tables.CUSTOMER.NAME, Tables.CUSTOMER.AGE).from(Tables.CUSTOMER).fetch();
+        Assert.assertEquals(Arrays.asList(12, 23, 26), customerList.getValues(2));
+    }
+
+    @Test
+    public void emptyRecordsFailure() {
+        dslContext.delete(Tables.CUSTOMER).execute();
+        Result<Record> result = dslContext.select().from(Tables.CUSTOMER).fetch();
+        Assert.assertEquals(3, result.size());
+    }
 }
